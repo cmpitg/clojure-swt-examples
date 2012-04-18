92d06e4c897f83ba838e7398a249b1cd4013b2ba
Dương "Yang" ヤン Nguyễn <cmpitg@gmail.com>
2012-04-18 16:01:38 +0700

    New example

---
+++ src/main/swt_styledtext_box_around.clj
@@ -0,0 +1,66 @@
+;;; FIXME: Refactor
+
+(ns main.swt-styledtext-box-around
+  (:import [org.eclipse.swt.widgets Display Shell]
+           [org.eclipse.swt.custom StyledText StyleRange]
+           [org.eclipse.swt.layout FillLayout]
+           [org.eclipse.swt SWT]))
+
+(defn gui-loop [display shell]
+  "Main loop for SWT application"
+  (when-not (.isDisposed shell)
+    (if-not (.readAndDispatch display)
+      (.sleep display))
+    (recur display shell)))
+
+(defn make-style-range-border-solid [display]
+  (let [sr (StyleRange.)]
+    (set! (. sr borderColor) (.getSystemColor display
+                                              (. SWT COLOR_RED)))
+    (set! (. sr borderStyle) (. SWT BORDER_SOLID))
+    sr))
+
+(defn make-style-range-array [styles]
+  (let [arr (make-array StyleRange 1)]
+    (aset arr 0 (first styles))
+    arr))
+
+(defn set-style-get-pos [text-area text search-str pos styles]
+  (.setStyleRanges text-area 0 0 (int-array 2 [pos (.length search-str)])
+                   (make-style-range-array styles))
+  (.indexOf text search-str (inc pos)))
+
+(defn draw-box [search-str text-area text styles]
+  (loop [pos (.indexOf text search-str)]
+    (if (not= -1 pos)
+      (recur (set-style-get-pos text-area text search-str pos styles))
+      nil)))
+
+(defn gui-main []
+  (let [search-str "box"
+        contents (str "This demonstrates drawing a box\n"
+                      "around every occurrence of the word\n"
+                      "box in the StyledText")
+
+        display (new Display)
+        shell   (doto (new Shell display)
+                  (.open)
+                  (.setText "StyledText with box drawn around")
+                  (.setLayout (FillLayout.)))
+
+        styles [(make-style-range-border-solid display)]
+
+        text-area (doto (StyledText. shell (. SWT NONE))
+                    (.setText contents))]
+    (draw-box search-str text-area contents styles)
+
+    (doto shell
+      (.pack)
+      (.open))
+
+    (gui-loop display shell)
+    (.dispose display)))
+
+(defn -main [& args]
+  (gui-main)
+  (println "Hello"))
